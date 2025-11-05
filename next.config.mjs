63cd0d9a06d22fff0b5102ea0a6a870c36a65f6a
anila2-1 <w1techy8@gmail.com>
2025-11-05 22:50:32 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,20 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  output: 'standalone',
-  serverExternalPackages: ['@payloadcms/db-mongodb', 'mongoose'],
+  webpack: (webpackConfig) => {
+    webpackConfig.resolve.extensionAlias = {
+      '.cjs': ['.cts', '.cjs'],
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.mjs': ['.mts', '.mjs'],
+    }
+
+    return webpackConfig
+  },
+  transpilePackages: ['@payloadcms/richtext-lexical'],
+  serverExternalPackages: ['@payloadcms/db-mongodb'],
+  env: {
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || 'http://localhost:3000',
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
